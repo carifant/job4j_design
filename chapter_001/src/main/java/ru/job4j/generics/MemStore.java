abe9754989025b15e45b26023364cd0948a60d33
carifant <carifant@mail.ru>
2020-05-06 08:40:45 +0300

    5.2.2. Реализовать Store<T extends Base>[#275632]

--- chapter_001/src/main/java/ru/job4j/generics/MemStore.java
+++ chapter_001/src/main/java/ru/job4j/generics/MemStore.java
@@ -8,12 +8,11 @@ public final class MemStore<T extends Base> implements Store<T> {
     private final List<T> mem = new ArrayList<>();
 
     int indexOf(String id) {
-        int index = 0;
+        int index = -1;
         for (int i = 0; i < mem.size(); i++) {
             if (mem.get(i).getId().equals(id)) {
                 index = i;
-            } else {
-                index = -1;
+                break;
             }
         }
         return index;
@@ -26,32 +25,30 @@ public final class MemStore<T extends Base> implements Store<T> {
 
     @Override
     public boolean replace(String id, T model) {
-        if (indexOf(id) == -1) {
+        int index = indexOf(id);
+        if (index == -1) {
             return false;
-        } else {
-            mem.add(indexOf(id), model);
-            return true;
         }
+        mem.add(indexOf(id), model);
+        return true;
     }
 
     @Override
     public boolean delete(String id) {
-        if (indexOf(id) == -1) {
+        int index = indexOf(id);
+        if (index == -1) {
             return false;
-        } else {
-            mem.remove(indexOf(id));
-            return true;
         }
+        mem.remove(indexOf(id));
+        return true;
     }
-
 
     @Override
     public T findById(String id) {
-        for (T t : mem) {
-            if (t.getId().equals(id)) {
-                return t;
-            }
+        int index = indexOf(id);
+        if (index == -1) {
+            return null;
         }
-        return null;
+        return mem.get(index);
     }
 }
