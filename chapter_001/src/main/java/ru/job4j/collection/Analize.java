2e9278618cb55418b2f3b9d6d547cead108e9006
carifant <carifant@mail.ru>
2020-05-16 15:01:28 +0300

    2. Статистику по коллекции.[#275608]

--- chapter_001/src/main/java/ru/job4j/collection/Analize.java
+++ chapter_001/src/main/java/ru/job4j/collection/Analize.java
@@ -1,30 +1,33 @@
 package ru.job4j.collection;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Analize {
 
     public Info diff(List<User> previous, List<User> current) {
         Info info = new Info(0, 0, 0);
-        int i = 0;
+        Map<Integer, User> previousMap = new HashMap<>();
         for (User user : previous) {
-            if (!current.contains(user)) {
-                info.deleted++;
-            } else {
-                if (!current.get(i).name.equals(user.name)) {
+            previousMap.put(user.id, user);
+        }
+
+        for (int l = 0; l < current.size(); l++) {
+            if (previousMap.containsKey(current.get(l).id)) {
+                if (!previousMap.get(current.get(l).id).name.equals(current.get(l).name)) {
                     info.changed++;
                 }
-                i++;
             }
+            previousMap.remove(current.get(l).id);
         }
-        for (User user : current) {
-            if (!previous.contains(user)) {
-                info.added++;
-            }
-        }
+        info.deleted = previousMap.size();
+
+        int i1 = previous.size() - info.deleted;
+        info.added = current.size() - i1;
         return info;
     }
-
 
     public static class User {
         int id;
