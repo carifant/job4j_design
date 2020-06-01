a32984a78d532c54d6e9f5b2fb275c2574eaf5c4
carifant <carifant@mail.ru>
2020-06-01 08:27:35 +0300

    2. Анализ доступности сервера.[859#275667]

--- chapter_002/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_002/src/main/java/ru/job4j/io/Analizy.java
@@ -9,34 +9,37 @@ public class Analizy {
             String line = reader.readLine();
             List<String> list = new ArrayList<>();
             while (line != null) {
-                if (line.length() == 0) {
-                    line = reader.readLine();
-                    continue;
-                }
-                if (line.contains("400") || line.contains("500")) {
-                    String[] temp = line.split(" ");
-                    list.add(temp[1] + "; ");
-                    line = reader.readLine();
-                    while (line != null && (!line.contains("200") || !line.contains("300"))) {
-                        if (line.contains("200") || line.contains("300")) {
-                            String[] temp2 = line.split(" ");
-                            list.add(temp2[1] + System.lineSeparator());
-                            break;
-                        }
+                if (line.length() != 0) {
+                    if (line.contains("400") || line.contains("500")) {
+                        String[] temp = line.split(" ");
+                        list.add(temp[1] + "; ");
                         line = reader.readLine();
+                        while (line != null && (!line.contains("200") || !line.contains("300"))) {
+                            if (line.contains("200") || line.contains("300")) {
+                                String[] temp2 = line.split(" ");
+                                list.add(temp2[1] + System.lineSeparator());
+                                break;
+                            }
+                            line = reader.readLine();
+                        }
                     }
                 }
                 line = reader.readLine();
             }
-            PrintWriter out = new PrintWriter(new FileOutputStream(target));
-            for (int i = 0; i < list.size(); i++) {
-                out.print(list.get(i));
-            }
-            out.close();
+            ReaderList(list, target);
         } catch (IOException ex) {
             ex.getStackTrace();
         }
     }
+
+    public void ReaderList(List<String> list, String target) throws FileNotFoundException {
+        PrintWriter out = new PrintWriter(new FileOutputStream(target));
+        for (int i = 0; i < list.size(); i++) {
+            out.print(list.get(i));
+        }
+        out.close();
+    }
+
 
     public static void main(String[] args) {
         new Analizy().unavailable("file.txt", "unavailable.csv");
