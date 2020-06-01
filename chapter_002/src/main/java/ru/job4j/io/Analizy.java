1ce5d313e0a76c1a1bf1a512a3791b435aabdce1
carifant <carifant@mail.ru>
2020-06-01 09:15:12 +0300

    3.0. Тестирование IO[173905#275660]

--- chapter_002/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_002/src/main/java/ru/job4j/io/Analizy.java
@@ -12,12 +12,12 @@ public class Analizy {
                 if (line.length() != 0) {
                     if (line.contains("400") || line.contains("500")) {
                         String[] temp = line.split(" ");
-                        list.add(temp[1] + "; ");
+                        list.add(temp[1] + " ");
                         line = reader.readLine();
                         while (line != null && (!line.contains("200") || !line.contains("300"))) {
                             if (line.contains("200") || line.contains("300")) {
                                 String[] temp2 = line.split(" ");
-                                list.add(temp2[1] + System.lineSeparator());
+                                list.add(temp2[1] + "; " + System.lineSeparator());
                                 break;
                             }
                             line = reader.readLine();
@@ -26,13 +26,13 @@ public class Analizy {
                 }
                 line = reader.readLine();
             }
-            ReaderList(list, target);
+            readerList(list, target);
         } catch (IOException ex) {
             ex.getStackTrace();
         }
     }
 
-    public void ReaderList(List<String> list, String target) throws FileNotFoundException {
+    public void readerList(List<String> list, String target) throws FileNotFoundException {
         PrintWriter out = new PrintWriter(new FileOutputStream(target));
         for (int i = 0; i < list.size(); i++) {
             out.print(list.get(i));
--- chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,31 +1,41 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Stream;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
-    public void when1() {
-        Analizy analizy = new Analizy();
-        analizy.unavailable("C:\\projects\\job4j_design\\file.txt", "C:\\projects\\job4j_design\\unavailable.csv");
-        List<String> list = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\projects\\job4j_design\\unavailable.csv"))) {
-            String s = reader.readLine();
-            while (s != null) {
-                list.add(s);
-                s = reader.readLine();
-            }
-        } catch (IOException e) {
-            e.getStackTrace();
+    public void when1() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01\n" +
+                    "500 10:57:01\n" +
+                    "400 10:58:01\n" +
+                    "200 10:59:01\n" +
+                    "500 11:01:01\n" +
+                    "200 11:02:01\n" +
+                    "200 11:03:01\n" +
+                    "300 11:04:01\n" +
+                    "400 11:05:01\n" +
+                    "300 11:06:01");
         }
-        List<String> result = List.of("10:57:01; 10:59:01", "11:01:01; 11:02:01", "11:05:01; 11:06:01");
-        assertThat(list, is(result));
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("10:57:01 10:59:01; 11:01:01 11:02:01; 11:05:01 11:06:01; "));
     }
 }
